3a8fc759829361a9b6159b701ac0958a080d1707
liu <l1401098954@gmail.com>
2023-12-05 13:25:44 +0800

    111

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+proxy = os.getenv('PROXY')
 proxies = {
-    'http': 'http://127.0.0.1:7890',
-    'https': 'http://127.0.0.1:7890',
+    'http': proxy,
+    'https': proxy,
 }
-# Set the API endpoint and access token
-api_endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-access_token = "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"
+# 指定模型的 API 端点
+api_endpoint = os.getenv('API_ENDPOINT')
 
-# Set the prompt text and parameters
-prompt_text = "Hello, how are you today?"
+# 提示文本和参数
+prompt_text = "我给你发一段描述，你帮我翻译成AI绘画能识别的单词；一个漂亮的女孩在沙滩晒太阳，天空晴朗，大海湛蓝色，卷起朵朵浪花"
 params = {
     "prompt": prompt_text,
     "temperature": 0.7,
@@ -20,14 +24,18 @@ params = {
     "presence_penalty": 0.0
 }
 
-# Send the API request
-headers = {"Content-Type": "application/json",
-           "Authorization": f"Bearer {access_token}"}
+# API 请求的头部信息
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+# 发送 API 请求
 response = requests.post(api_endpoint, headers=headers, json=params, proxies=proxies)
 
-# Process the API response
+# 处理 API 响应
 if response.status_code == 200:
     response_text = json.loads(response.text)["choices"][0]["text"]
-    print(f"ChatGPT response: {response_text}")
+    print(f"ChatGPT 响应: {response_text}")
 else:
-    print(f"Error: {response.status_code} - {response.text}")
+    print(f"错误: {response.status_code} - {response.text}")
